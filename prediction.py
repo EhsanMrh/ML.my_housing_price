c8534052443d6458a5a6014b11f62153b5e9b770
NOOR <ehsanmirhashemi41@gmail.com>
2020-08-17 00:27:29 +0430

    Add Box plot on dataset

--- prediction.py
+++ prediction.py
@@ -31,3 +31,23 @@ dataset = pd.read_csv('housing_dataset.csv', names=columns, delimiter=r"\s+")
 print(dataset.head(6))
 print(np.shape(dataset))
 print(dataset.describe())
+
+# Visualizing the dataset
+import matplotlib.pyplot as plt
+# Box Plot
+import seaborn as sns 
+
+fig, axs = plt.subplots(ncols=7, nrows=2, figsize=(20, 10))
+index = 0
+axs = axs.flatten()
+
+for k, v in dataset.items():
+    sns.boxplot(y = k, data = dataset, ax = axs[index])
+    index += 1
+
+plt.tight_layout(pad = 0.4, w_pad = 0.5, h_pad = 0.5)
+
+# Remove MEDV Outliers
+dataset = dataset[~(dataset["MEDV"] >= 50.0)]
+print(np.shape(dataset))
+
