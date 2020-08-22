8e02bd955d968fbf7efb8aee96614cf37445c9eb
NOOR <ehsanmirhashemi41@gmail.com>
2020-08-22 14:06:21 +0430

    Add Scatter Plot

--- prediction.py
+++ prediction.py
@@ -51,3 +51,25 @@ plt.tight_layout(pad = 0.4, w_pad = 0.5, h_pad = 0.5)
 dataset = dataset[~(dataset["MEDV"] >= 50.0)]
 print(np.shape(dataset))
 
+# Scatter plot
+from sklearn.preprocessing import MinMaxScaler
+# Let's scale the columns before plotting them against MEDV
+min_max_scaler = MinMaxScaler()
+
+column_sels = ['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
+x = dataset.loc[:,column_sels]
+y = dataset['MEDV']
+x = pd.DataFrame(data=min_max_scaler.fit_transform(x), columns=column_sels)
+
+fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(20, 10))
+index = 0
+axs = axs.flatten()
+for i, k in enumerate(column_sels):
+    sns.regplot(y=y, x=x[k], ax=axs[i])
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=5.0)
+
+# Remove the skewness of the data trough log transformation
+y =  np.log1p(y)
+for col in x.columns:
+    if np.abs(x[col].skew()) > 0.3:
+        x[col] = np.log1p(x[col])
